d73344fa91fa0c6ed2860a440ad1eef467da5c49
frivolta <rivoltafilippo@gmail.com>
2019-08-19 22:59:53 +0200

    :sparkles: Adding Google OAuth Passport and Nodemon

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 //@ToDo: Setup ESLint and Prettier
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
+// Setup app
 const app = express();
 
+// Setup Passport Google OAuth 2.0 strategy
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('Access token: ', accessToken)
+  console.log('Refresh token: ', refreshToken)
+  console.log('Profile: ', profile)
+}));
+
+// Authentication Route
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+// Authentication route callback with passport google code
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 /* 
 @ToDo: Promise based get request to improve performance,
@@ -22,9 +44,7 @@ getPaths();
 
 */
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'Buddy' })
-})
+
 // Env variables are injected by Heroku
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
