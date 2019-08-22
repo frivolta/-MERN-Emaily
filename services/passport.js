5497d4dbdffbadb65f8f7b0b7e7d1fe6038ff48a
frivolta <rivoltafilippo@gmail.com>
2019-08-23 00:39:42 +0200

    :sparkles: Adding redux, router

--- services/passport.js
+++ services/passport.js
@@ -22,16 +22,13 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
+}, async (accessToken, refreshToken, profile, done) => {
   // Create user
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
-      if (existingUser) {
-        // we already have a record with the given ID
-        done(null, existingUser);
-      } else {
-        new User({ googleId: profile.id }).save()
-          .then(user => done(null, user));
-      }
-    })
+  const existingUser = await User.findOne({ googleId: profile.id })
+  if (existingUser) {
+    // we already have a record with the given ID
+    return done(null, existingUser);
+  }
+  const user = await new User({ googleId: profile.id }).save()
+  done(null, user)
 }));
