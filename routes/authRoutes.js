1ff5c954300ee9dc7ab20a598ff50a4eb3e56e82
frivolta <rivoltafilippo@gmail.com>
2019-08-24 12:38:45 +0200

    :sparkles: Adding auth routes and reducers

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,12 +7,14 @@ module.exports = app => {
   }))
 
   // Authentication route callback with passport google code
-  app.get('/auth/google/callback', passport.authenticate('google'))
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+    res.redirect('/surveys')
+  })
 
   // Logout user
   app.get('/api/logout', (req, res) => {
     req.logout();
-    res.send(req.user)
+    res.redirect('/')
   })
   // Test current logged user
   app.get('/api/current_user', (req, res) => {
